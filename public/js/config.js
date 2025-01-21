9d6a913bf48b57f726e4e00b09844b2b42a5dffc
Alfred <alfredkangjr@gmail.com>
2025-01-21 21:48:46 +0800

    Initial commit

---
+++ public/js/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzp0IjOK96yFnon3uySgOicKyPJci63fA",
+  authDomain: "intheirshoesvr.firebaseapp.com",
+  projectId: "intheirshoesvr",
+  storageBucket: "intheirshoesvr.firebasestorage.app",
+  messagingSenderId: "217693075201",
+  appId: "1:217693075201:web:ed2060a99265b9e0e04d00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ public/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        'grotesk': ['Hanken Grotesk', 'sans-serif'],
+        'garet': ['Garet', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          'pgreen': '#94ffbd',
+          'pgrey': '#e7e7e7',
+          'pblue': '#a8eeff',
+          'pred': '#ff8a8a',
+          'pblack': '#313131',
+        },
+      },
+    }
+  }
