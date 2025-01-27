46344fed71b83fdc02367ae9b1b4682ee0eadf40
Alfred-Kang <alfredkangjr@gmail.com>
2025-01-27 09:46:42 +0800

    feat: accessibility optimisations

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -7,11 +7,13 @@ tailwind.config = {
       },
       extend: {
         colors: {
-          'pgreen': '#94ffbd',
-          'pgrey': '#e7e7e7',
-          'pblue': '#a8eeff',
-          'pred': '#ff8a8a',
-          'pblack': '#313131',
+          'aorange': '#e69f00',
+          'agreen': '#2b9f78',
+          'alblue': '#56b4e9',
+          'adblue': '0072b2',
+          'ayellow': '#foe442',
+          'ared': '#d55e00',
+          'apink': '#cc79a7',
         },
       },
     }
