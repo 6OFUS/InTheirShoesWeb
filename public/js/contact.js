1e4010facf9f1418b3a3f66a953c5f2e90f28054
Alfred-Kang <alfredkangjr@gmail.com>
2025-02-16 10:46:18 +0800

    fix: contact mobile hamburger

--- public/js/contact.js
+++ public/js/contact.js
@@ -24,9 +24,3 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     });
 });
-
-// mobile nav
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.classList.toggle('hidden');
-});
